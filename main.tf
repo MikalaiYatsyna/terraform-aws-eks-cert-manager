417c53dc50511c45dd266fbd54e617567dec5d43
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 22:11:59 +0100

    Pin versions
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "cert_manager" {
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
+  version    = "1.14.3"
   name       = "${var.stack}-cert-manager"
   namespace  = var.namespace
   atomic     = true
