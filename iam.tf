942d915ecc0af582f9383ab33cd78ab5901a4f32
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-22 19:56:11 +0100

    Get rid of AWS provider
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- iam.tf
+++ iam.tf
@@ -1,28 +1,8 @@
-locals {
-  service_account_name = "cert-manager-sa"
-}
-
-module "cert_manager_role" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name                     = "${var.stack}-cert-manager-role"
-  attach_cert_manager_policy    = true
-  cert_manager_hosted_zone_arns = [data.aws_route53_zone.zone.arn]
-  oidc_providers = {
-    main = {
-      provider_arn               = var.oidc_provider_arn
-      namespace_service_accounts = ["${var.namespace}:${local.service_account_name}"]
-    }
-  }
-}
-
 resource "kubernetes_service_account" "cert-manager-sa" {
   metadata {
-    name      = local.service_account_name
+    name      = var.service_account_name
     namespace = var.namespace
-    annotations = {
-      "eks.amazonaws.com/role-arn"               = module.cert_manager_role.iam_role_arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
-    }
+    annotations = var.service_account_annotations
   }
   secret {
     name = "${var.stack}-cert-manager-sa-secret"
--- variables.tf
+++ variables.tf
@@ -3,22 +3,39 @@ variable "stack" {
   description = "Stack name"
 }
 
-variable "cluster_name" {
-  type        = string
-  description = "Name of EKS cluster"
-}
-
-variable "domain" {
-  type        = string
-  description = "Root application domain name"
-}
-
 variable "namespace" {
   type        = string
   description = "Namespace for Vault release"
 }
 
-variable "oidc_provider_arn" {
+variable "cluster_endpoint" {
+  sensitive   = true
   type        = string
-  description = "OIDC provider arn"
+  description = "Endpoint of the cluster."
+}
+
+variable "cluster_ca" {
+  sensitive   = true
+  type        = string
+  description = "CA certificate of the cluster."
+}
+
+variable "k8s_exec_args" {
+  type        = list(string)
+  description = "Args for Kubernetes provider exec plugin. Example command ['eks', 'get-token', '--cluster-name', '{clusterName}}']."
+}
+
+variable "k8s_exec_command" {
+  type        = string
+  description = "Command name for Kubernetes provider exec plugin. Example - 'aws."
+}
+
+variable "service_account_name" {
+  type = string
+  description = "Kubernetes service account name, to be created and used for cert-manager."
+}
+
+variable "service_account_annotations" {
+  type = map(string)
+  description = "Annotations to be applied to service account. e.g 'eks.amazonaws.com/sts-regional-endpoints' = 'true'."
 }
